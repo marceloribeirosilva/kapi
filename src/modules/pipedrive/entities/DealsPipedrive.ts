2ee30388a81a78682924728d7f5fbda2235867ed
Marcelo Silva <devel_02@autoavaliar.com.br>
2021-07-14 20:31:52 -0300

    chore: add service pipedrive

---
+++ src/modules/pipedrive/entities/DealsPipedrive.ts
@@ -0,0 +1,16 @@
+export default class DealsPipedrive {
+    id: number;
+  
+    org_id: {
+      name: string;
+      cc_email: string;
+    };
+  
+    title: string;
+
+    value: number;
+
+    add_time: string;
+
+    update_time: string;
+  }
---
+++ src/modules/pipedrive/services/GetAllDealsService.ts
@@ -0,0 +1,36 @@
+import AppError from '@shared/errors/AppError';
+import axios from 'axios';
+import DealsPipedrive from "../entities/DealsPipedrive";
+
+class GetAllDealsService {
+    public async execute(): Promise<DealsPipedrive[]> {
+        const deals: DealsPipedrive[] = [];
+        try {
+            const urlBase = process.env.ENDPOINT_PIPEDRIVE;
+            const apiKey = process.env.ENDPOINT_PIPEDRIVE;
+            const response = await axios.get(`${urlBase}/deals?status=won&api_token=${apiKey}`);
+
+            if (response && response.data) {
+                for (let item of response.data) {
+                    deals.push({
+                        id: item.id,
+                        add_time: item.add_time,
+                        update_time: item.update_time,
+                        title: item.title,
+                        value: item.value,
+                        org_id: {
+                            name: item.org_id.name,
+                            cc_email: item.org_id.cc_email,
+                        }
+                    })
+                }
+            }            
+        } catch (err) {
+            throw new AppError('error to get deals', 400);
+        }
+
+        return deals
+    }    
+}
+
+export default GetAllDealsService;
