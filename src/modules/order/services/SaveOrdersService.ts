7de74ab4299fd1e0ff4f7db13723018465aa4259
Marcelo Silva <devel_02@autoavaliar.com.br>
2021-07-15 15:51:16 -0300

    refactor: remove variable

--- src/modules/order/services/SaveOrdersService.ts
+++ src/modules/order/services/SaveOrdersService.ts
@@ -3,7 +3,7 @@ import Order from "../entities/Order";
 
 class SaveOrdersService {
     public async execute(client: MongoClient, orders: Order[]) {
-        const result = await client.db("teste").collection("vendas").insertMany(orders);        
+        await client.db("teste").collection("vendas").insertMany(orders);        
     }
 }
 
